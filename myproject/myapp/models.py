df364e42bc41fbc666580e1537b014d06702e65d
Bipin Paudel <paudelbipin19@gmail.com>
2025-06-04 23:30:13 +0545

    day 04 of learnning

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    joined_date = models.DateField(auto_now_add=True)
 
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
